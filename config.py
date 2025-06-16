0ffe04200aafcde32af02fb0af3ecd3f110950d3
xdweapon36 <143059981+xdweapon36@users.noreply.github.com>
2025-06-16 11:31:52 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+DISCORD_TOKEN = "MTM4Mzk5OTYyMzM4Njg5NDQyOA.G7APpn.9FUsYRNiyagvucg7KPFdeGAqM0W5eA7V2ONWvY"
+
+CHANNEL_IDS = {
+    channelid1: "Stock",
+    channelid2: "Weather",
+    channelid3: "Egg Stock",
+    channelid4: "Honey Stock"
+}
+
+NTFY_TOPIC = "Gag notifyer"
